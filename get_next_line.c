3deef4d04acdeb02466e659744a344d08e09fccb
Denis ZHENG <dzheng@e3r13p1.42.fr>
2016-12-23 11:01:06 +0100

    last version w/ multi fd

--- get_next_line.c
+++ get_next_line.c
@@ -6,14 +6,13 @@
 /*   By: dzheng <dzheng@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/12/14 19:23:08 by dzheng            #+#    #+#             */
-/*   Updated: 2016/12/20 17:49:05 by dzheng           ###   ########.fr       */
+/*   Updated: 2016/12/23 10:59:38 by dzheng           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
-#include <stdio.h>
 
-int				checkendofline(char **save, char **line)
+static int		checkendofline(char **save, char **line)
 {
 	char		*tmp;
 	int			i;
@@ -21,46 +20,62 @@ int				checkendofline(char **save, char **line)
 	i = 0;
 	tmp = NULL;
 	if (*save)
-	{
 		if (ft_strchr(*save, '\n') != NULL)
 		{
 			tmp = ft_strchr(*save, '\n');
-			*tmp = '\0';
+			tmp[0] = '\0';
 			while (save[i])
 			{
 				if (*save[i] == '\n')
 					*save[i] = '\0';
 				i++;
 			}
-			*line = ft_strdup(*save);
-			*save = ft_strdup(&tmp[1]);
-			tmp = NULL;
-			free(tmp);
+			if (!(*line = ft_strdup(*save)))
+				return (0);
+			if (!(*save = ft_strdup(&tmp[1])))
+				return (0);
+			return (1);
+		}
+	return (0);
+}
+
+static int		whileread(const int fd, char *buf, char **save, char **line)
+{
+	int			ret;
+	char		*ptr;
+
+	while ((ret = read(fd, buf, BUFF_SIZE)) > 0)
+	{
+		buf[ret] = '\0';
+		ptr = ft_strjoin(*save, buf);
+		if (!ptr)
+			return (-1);
+		free(*save);
+		*save = ptr;
+		if (checkendofline(save, line))
+		{
+			free(ptr);
 			return (1);
 		}
 	}
+	if (ret < 0)
+		return (-1);
 	return (0);
 }
 
 int				get_next_line(const int fd, char **line)
 {
-	int			ret;
 	char		buf[BUFF_SIZE + 1];
 	static char	*save;
 
 	if (checkendofline(&save, line))
 		return (1);
 	if (!save)
-		save = ft_strnew(0);
-	while ((ret = read(fd, buf, BUFF_SIZE)))
-	{
-		if (fd == -1)
-			return (-1);
-		buf[ret] = '\0';
-		save = ft_strjoin(save, buf);
-		if (checkendofline(&save, line))
-			return (1);
-	}
+		save = ft_strnew(1);
+	if (whileread(fd, buf, &save, line) > 0)
+		return (1);
+	else if (whileread(fd, buf, &save, line) < 0)
+		return (-1);
 	if (save)
 	{
 		*line = ft_strdup(save);
@@ -68,41 +83,6 @@ int				get_next_line(const int fd, char **line)
 			return (0);
 		save = NULL;
 		return (1);
-	}
-	return (0);
-}
-
-int				main(int ac, char *av[])
-{
-	int			fd;
-	char		*line;
-	int			i;
-
-	i = 0;
-	line = NULL;
-	if (ac != 2)
-	{
-		ft_putstr("usage: get next line input\n");
-	}
-	else
-	{
-		fd = open(av[1], O_RDONLY);
-		if (fd == -1)
-		{
-			ft_putstr("open error");
-			return (1);
-		}
-		while (i < 20)
-		{
-			get_next_line(fd, &line);
-			printf("\x1b[32m%s\n\x1b[0m", line);
-			i++;
-		}
-		if (close(fd) == -1)
-		{
-			ft_putstr("close error");
-			return (1);
-		}
 	}
 	return (0);
 }
--- main.c
+++ main.c
@@ -1,26 +1,34 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: dzheng <dzheng@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2016/12/14 19:23:20 by dzheng            #+#    #+#             */
-/*   Updated: 2016/12/23 10:59:56 by dzheng           ###   ########.fr       */
+/*   Created: 2016/12/22 18:01:48 by dzheng            #+#    #+#             */
+/*   Updated: 2016/12/23 10:04:31 by dzheng           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef GET_NEXT_LINE_H
-# define GET_NEXT_LINE_H
+#include "get_next_line.h"
+#include <stdio.h>
 
-# include <sys/types.h>
-# include <sys/stat.h>
-# include <fcntl.h>
-# include "libft/libft.h"
-# include <string.h>
+int				main(int ac, char *av[])
+{
+	int			fd;
+	char		*line;
+	int			i;
 
-# define BUFF_SIZE 100000
-
-int		get_next_line(const int fd, char **line);
-
-#endif
+	i = 0;
+	line = NULL;
+	fd = open(av[1], O_RDONLY);
+	if (ac != 2)
+		ft_putstr("ac != 2\n");
+	while (i < 20)
+	{
+		printf("Return value = %i\n", get_next_line(fd, &line));
+		printf("\x1b[32m%s\n\x1b[0m", line);
+		i++;
+	}
+	return (0);
+}
